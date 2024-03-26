b30a576433f3137c8e7d1dab121f995262574315
Kamaldulal <153346700+Kamaldulal@users.noreply.github.com>
2024-03-26 08:48:46 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('proposalBtn').addEventListener('click', function() {
+    var responseDiv = document.getElementById('response');
+    responseDiv.textContent = "Take Your Time, I can wait forever !";
+});
